4729ac832d52e69f13b8e7ed944007eba093c516
Matthew Howells <m.howells@blackarrowgroup.io>
2023-08-01 14:24:06 +0100

    add application class

---
+++ app/src/main/java/uk/rootmu/chatapplication/ChatApplication.kt
@@ -0,0 +1,15 @@
+package uk.rootmu.chatapplication
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class ChatApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@ChatApplication)
+        }
+    }
+}
