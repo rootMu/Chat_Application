3c774136f879974a997fbb131cf51fdf5ff5d490
Matthew Howells <m.howells@blackarrowgroup.io>
2023-08-01 14:59:19 +0100

    fix java inconsistency and consolidate redundant Entity Class

--- app/src/main/java/uk/rootmu/chatapplication/data/local/model/Message.kt
+++ app/src/main/java/uk/rootmu/chatapplication/data/local/model/Message.kt
@@ -1,15 +1,15 @@
 package uk.rootmu.chatapplication.data.local.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "message")
 data class Message(
     val content: String,
     val sender: String,
     val recipient: String,
     val timestamp: Long
-)
-
-val Message.MessageToEntity get() = MessageEntity(
-    this.content,
-    this.sender,
-    this.recipient,
-    this.timestamp
-)
+)  {
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
+}
