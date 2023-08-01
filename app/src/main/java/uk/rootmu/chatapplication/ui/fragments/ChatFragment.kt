92cddb444a4edf96836b5de0fd41a87c1970f816
Matthew Howells <m.howells@blackarrowgroup.io>
2023-08-01 16:28:30 +0100

    starting to implement ui
    chat fragment ui initial implementation

--- app/src/main/java/uk/rootmu/chatapplication/ui/fragments/ChatFragment.kt
+++ app/src/main/java/uk/rootmu/chatapplication/ui/fragments/ChatFragment.kt
@@ -1,19 +1,18 @@
-package uk.rootmu.chatapplication
+package uk.rootmu.chatapplication.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import uk.rootmu.chatapplication.databinding.FragmentFirstBinding
+import androidx.fragment.app.Fragment
+import uk.rootmu.chatapplication.databinding.FragmentChatBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A chat [Fragment] subclass that displays a list of messages and allows more to entered.
  */
-class FirstFragment : Fragment() {
+class ChatFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentChatBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,9 +21,9 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentChatBinding.inflate(inflater, container, false)
         return binding.root
 
     }
